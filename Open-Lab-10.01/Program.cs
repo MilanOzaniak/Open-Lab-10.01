e2fb3dc71d713dde7868d1476bb73584d1a8ca56
Ozaniak <Ozaniak@PC>
2020-11-10 01:54:01 +0100

    hotovo

--- Open-Lab-10.01/Program.cs
+++ Open-Lab-10.01/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Text;
 
 namespace Open_Lab_10._01
 {
     class Program
     {
+        public class Book
+        {
+            public string Title;
+            private int Pages;
+
+            public void SetPages(int pages)
+            {
+                Pages = pages;
+            }
+
+            public int GetPages()
+            {
+                return Pages;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book book = new Book();
+            book.Title = "Lord of The Rings";
+            book.SetPages(300);
+
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.GetPages());
+
         }
     }
 }
